fe07518d18f0c037eb72c3a94ab90b50168ad316
Tiago M Silva <tiagomms.15@gmail.com>
2025-05-21 23:57:19 +0100

    always showing bpm even though it is not working

--- Assets/Scripts/AudioSystem/TempoController.cs
+++ Assets/Scripts/AudioSystem/TempoController.cs
@@ -14,6 +14,14 @@ namespace AudioSystem
 
         private OnsetBpmAndTimeSignatureEstimator _estimator;
         private bool _isInitialized;
+        private float _disableCollisionTimer = 0f;
+
+        // To reset so that it does not invoke too many times in a row.
+        private void Update()
+        {
+            if (_disableCollisionTimer < 2f)
+                _disableCollisionTimer += Time.deltaTime;
+        }
 
         private void Awake()
         {
@@ -27,7 +35,7 @@ namespace AudioSystem
             // Initialize the estimator service
             // TODO: Replace with dependency injection
             _estimator = new OnsetBpmAndTimeSignatureEstimator();
-            
+
             // Ensure text is disabled initially
             tempoText.gameObject.SetActive(false);
             _isInitialized = true;
@@ -36,6 +44,8 @@ namespace AudioSystem
         private void OnTriggerEnter(Collider other)
         {
             if (!_isInitialized) return;
+            // disable collisions at start
+            if (_disableCollisionTimer < 2f) return;
 
             // Check if the colliding object has a RecordAudioInterface in its hierarchy
             var audioInterface = other.transform.parent.GetComponent<RecordAudioInterface>();
@@ -53,25 +63,31 @@ namespace AudioSystem
             var (bpm, beatsPerBar) = _estimator.Estimate(samples);
 
             // Update the text display
-            /*
+            
             if (bpm > 0)
             {
                 tempoText.text = $"4/4\n{bpm} bpm";
                 tempoText.gameObject.SetActive(true);
             }
-            */
-            tempoText.text = $"4/4\n{bpm} bpm";
+            
+            // FIXME: does not work estimate
+            tempoText.text = "Coming Soon";//$"4/4\n{bpm} bpm"; 
             tempoText.gameObject.SetActive(true);
             XRDebugLogViewer.Log($"[{nameof(TempoController)}] bpm: {bpm}; beats per bar: {beatsPerBar}");
-
+            _disableCollisionTimer = 0f; // reset timer
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (!_isInitialized) return;
+            // Check if the colliding object has a RecordAudioInterface in its hierarchy
+            var audioInterface = other.transform.parent.GetComponent<RecordAudioInterface>();
+            if (audioInterface == null) return;
+            _disableCollisionTimer = 0f; // reset timer
+
 
             // Hide the text when the object leaves the trigger
             //tempoText.gameObject.SetActive(false);
         }
     }
-} 
+}
